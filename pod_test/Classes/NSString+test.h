6bff3d03c8f4860fb8d506e0e771e94f8e6f595a
chenxiaojun <>
2019-09-12 15:04:18 +0800

    diyicitijiao

---
+++ pod_test/Classes/NSString+test.h
@@ -0,0 +1,20 @@
+//
+//  NSString+test.h
+//  pod_test_Example
+//
+//  Created by chenxiaojun on 2019/9/12.
+//  Copyright © 2019 chenxiaojun99le@126.com. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (test)
+
+-(void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pod_test/Classes/NSString+test.m
@@ -0,0 +1,15 @@
+//
+//  NSString+test.m
+//  pod_test_Example
+//
+//  Created by chenxiaojun on 2019/9/12.
+//  Copyright © 2019 chenxiaojun99le@126.com. All rights reserved.
+//
+
+#import "NSString+test.h"
+
+@implementation NSString (test)
+-(void)test{
+    NSLog(@"cxj-test");
+}
+@end
